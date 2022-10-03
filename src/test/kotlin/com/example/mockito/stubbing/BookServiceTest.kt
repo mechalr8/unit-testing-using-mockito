2aa676fb7762f5ec1f17e070e432ce35f20c5a49
mechalr8 <harsitagrawal59@gmail.com>
2022-10-03 19:41:55 +0530

    using when only once while using thenReturn multiple times to get multiple values

--- src/test/kotlin/com/example/mockito/stubbing/BookServiceTest.kt
+++ src/test/kotlin/com/example/mockito/stubbing/BookServiceTest.kt
@@ -24,17 +24,20 @@ class BookServiceTest {
     fun textCalculateTotalCostOfBooks(){
         val bookIds = mutableListOf<String>()
         bookIds.add("1234")
-        bookIds.add("1235")
+        bookIds.add("1234")
 
         val book1 = Book("1234", "Mockito in Action", 530, LocalDate.now().toString())
         val book2 = Book("1235", "JUnit 5 in Action", 480, LocalDate.now().toString())
 
-//        `when`(bookRepository.findBookByBookId("1234")).thenReturn(book1)
+        `when`(bookRepository.findBookByBookId("1234"))
+            .thenReturn(book1)
+            .thenReturn(book1)
 //        `when`(bookRepository.findBookByBookId("1235")).thenReturn(book2)
 
-        doReturn(book1).`when`(bookRepository).findBookByBookId("1234")
-        doReturn(book2).`when`(bookRepository).findBookByBookId("1235")
+//        doReturn(book1).`when`(bookRepository).findBookByBookId("1234")
+//        doReturn(book2).`when`(bookRepository).findBookByBookId("1235")
+
         val actualCost = bookService.calculateTotalCost(bookIds)
-        assertEquals(1010, actualCost)
+        assertEquals(1060, actualCost)
     }
 }
